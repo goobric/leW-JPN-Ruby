c3dcc8dfe195ddb78c43cca942fe2af914f04a75
goobric aka Mikal <goobric@gmail.com>
2022-05-17 15:27:30 +0200

    code for fizzbuzz

--- 03HmeWrk/fizzbuzz.rb
+++ 03HmeWrk/fizzbuzz.rb
@@ -4,6 +4,21 @@
 # if the integar is divisible by 15 (modulus % 15) print FizzBuzz
 # elseif integar is divisible by 5 (modulus % 5) print Buzz
 # elseif integar is divisible by 3 (modulus % 3) print Fizz
+# else print the value of the integar
+
+# range of 1 to 30
+
+(1..30).each do |num|
+    if num % 3 == 0 && num % 5 == 0
+        puts 'fizzbuzz'
+    elsif num % 5 == 0
+        puts 'buzz'
+    elsif num % 3 == 0
+        puts 'fizz'
+    else
+        puts num
+    end
+end
 
 
 
