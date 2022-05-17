f79609c270e18d3f95f3a3f0150ace0e186a8837
goobric aka Mikal <goobric@gmail.com>
2022-05-17 15:20:02 +0200

    define rules of FizzBuzz

---
+++ 03HmeWrk/fizzbuzz.rb
@@ -0,0 +1,9 @@
+###FizzBuzz
+
+# create a program that will loop over a series of integars
+# if the integar is divisible by 15 (modulus % 15) print FizzBuzz
+# elseif integar is divisible by 5 (modulus % 5) print Buzz
+# elseif integar is divisible by 3 (modulus % 3) print Fizz
+
+
+
