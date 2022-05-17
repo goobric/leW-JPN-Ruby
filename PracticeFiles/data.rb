53b294b115d4f5552c229323c09de8f0dc15e1eb
goobric aka Mikal <goobric@gmail.com>
2022-05-17 11:08:31 +0200

    added 2 variables: string & int

---
+++ PracticeFiles/data.rb
@@ -0,0 +1,10 @@
+# information is contained within a special data type called a 'variable'
+# variable names need to be specific
+
+character_name = "Jane"  #this is a string data type
+character_age = 35          #this is an integar data type
+
+puts ("There once was a person named" + character_name)
+puts ("they were"  + character_age "years old.")
+puts ("They really liked the name " + character_name)
+puts ("but didn't like being " + character_age)
