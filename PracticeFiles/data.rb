4f2fc9ba7942fb896598d6ec3b172d98b8a8afca
goobric aka Mikal <goobric@gmail.com>
2022-05-17 11:16:12 +0200

    str int bool float nil

--- PracticeFiles/data.rb
+++ PracticeFiles/data.rb
@@ -5,6 +5,16 @@ character_name = "Jane"  #this is a string data type
 character_age = 35          #this is an integar data type
 
 puts ("There once was a person named" + character_name)
-puts ("they were"  + character_age "years old.")
+puts ("they were"  + character_age + "years old.")
 puts ("They really liked the name " + character_name)
 puts ("but didn't like being " + character_age)
+
+name = "Mika"
+occupation ="Software Engineer"
+age = 55
+children = 2.5 #float point number contains a decimal point
+
+istall = false      #boolean true or false
+ishuman = true  #bloolean true or false
+
+flaws = nil #nil this has no value
