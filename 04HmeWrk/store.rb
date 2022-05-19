aef7cd377101658dcb6b6c54fa9f932b04d192e6
goobric aka Mikal <goobric@gmail.com>
2022-05-19 17:41:30 +0200

    hash data set

--- 04HmeWrk/store.rb
+++ 04HmeWrk/store.rb
@@ -2,3 +2,13 @@
 ## you own a shop that sells a variety of items
 ## create a data set using the hash notation
 
+market_item_prices = {
+    'burger' => 500,
+    'chicken' => 750,
+    'sushi' => 1000,
+    'oreos' => 250,
+    'fries' => 400,
+    'noodles' => 800,
+    'bubble tea' => 600
+}
+
