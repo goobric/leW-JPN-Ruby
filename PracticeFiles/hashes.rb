3e3b09334c58de2841b2e6df6093b9ee016ff3d0
goobric aka Mikal <goobric@gmail.com>
2022-05-19 15:25:14 +0200

    a 2D array of items & prices

--- PracticeFiles/hashes.rb
+++ PracticeFiles/hashes.rb
@@ -1,2 +1,8 @@
 ### HASH CRUD
 
+# array of items and prices
+market_items = [['burger', 500], ['chicken', 750], ['sushi', 1000], ['oreos', 250], ['fries', 400], ['noodles', 800], ['bubble tea', 600]]
+
+market_items = ['burger', 'chicken', 'sushi', 'oreos', 'fries', 'noodles', 'bubble tea']
+
+p market_items
