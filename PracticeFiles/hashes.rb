f4bc02306372c4e452511b9103c5cba638c76b19
goobric aka Mikal <goobric@gmail.com>
2022-05-19 17:22:10 +0200

    CRUD for hash

--- PracticeFiles/hashes.rb
+++ PracticeFiles/hashes.rb
@@ -22,22 +22,43 @@ market_item_prices = {
     'bubble tea' => 600
 }
 
+### CRUD practice with Hash
+
+# Create use the hash_name[new_key] = new_value
+market_item_prices['raman'] = 300
+
+# Read  use the hash_name(key)
+market_item_prices['sushi']
+market_item_prices.keys
+market_item_prices.values
+p market_item_prices.key?('apple')
+p market_item_prices.value?(800)
+
+# Update use the same Create process  : hash_name[key] = new_value
+market_item_prices['noodles'] = 760
+
+# Delete similar the Array process : use the .delete(key) method with the Key Value in the parenthesis
+market_item_prices.delete('burger')
+
+market_item_prices
+
+
 ### CRUD practice with Arrays
-market_items = ['burger', 'chicken', 'sushi', 'oreos', 'fries', 'noodles', 'bubble tea']
+#market_items = ['burger', 'chicken', 'sushi', 'oreos', 'fries', 'noodles', 'bubble tea']
 
 # Create use the .push() or the shovel <<
-market_items.push('passion fruit')
-market_items << 'cherry'
+#market_items.push('passion fruit')
+#market_items << 'cherry'
 
 # Read use the [index] square brackets with an index value
-market_items[2]
+#market_items[2]
 
 # Update use the [index] square brackets with an index value and assignment operator = 
-market_items[1] ='grilled chicken'
+#market_items[1] ='grilled chicken'
 
 # Delete two ways using the method .delete('object') or the .delete_at(index)
-market_items.delete('passion fruit')
-market_items.delete_at(-1)
+#market_items.delete('passion fruit')
+#market_items.delete_at(-1)
 
-p market_items
+#p market_items
 
