951276083944b52db6cc33f4055300bc3fb177a5
goobric aka Mikal <goobric@gmail.com>
2022-05-20 14:44:09 +0200

    ask for quantity store in variable

--- 04HmeWrk/store.rb
+++ 04HmeWrk/store.rb
@@ -2,29 +2,29 @@
 ## you own a shop that sells a variety of items
 ## create a data set using the hash notation
 
-market_item_prices = {
-    'burger' => 500,
-    'chicken' => 750,
-    'sushi' => 1000,
-    'oreos' => 250,
-    'fries' => 400,
-    'noodles' => 800,
-    'bubble tea' => 600
-}
+# market_item_prices = {
+#     'burger' => 500,
+#     'chicken' => 750,
+#     'sushi' => 1000,
+#     'oreos' => 250,
+#     'fries' => 400,
+#     'noodles' => 800,
+#     'bubble tea' => 600
+# }
 
-# add an item to the inventory
+# # add an item to the inventory
 
-market_item_prices['raman'] = 900
+# market_item_prices['raman'] = 900
 
-market_item_prices
+# market_item_prices
 
-# hash.each do |key, value|
-puts "These are the items in our store:"
+# # hash.each do |key, value|
+# puts "These are the items in our store:"
 
-market_item_prices.each do |item, price|
-    puts "#{item.capitalize}: $#{price}"
-end
-# hash is a {placeholder}
+# market_item_prices.each do |item, price|
+#     puts "#{item.capitalize}: $#{price}"
+# end
+# # hash is a {placeholder}
 
 market_items = {
     'burger' => 500,
@@ -33,10 +33,10 @@ market_items = {
     'oreos' => 250,
     'fries' => 400,
     'noodles' => 800,
-    'bubble tea' => 600
-    'raman' => 900
-    'onions' => 150
-    'cabbage' => 150
+    'bubble tea' => 600,
+    'raman' => 900,
+    'onions' => 150,
+    'cabbage' => 150,
     'rice' => 200
 }
 
@@ -58,7 +58,12 @@ market_items.each do |item, price|
 end
 
 puts "What would you like to buy?"
-item = gets.chomp
+user_item = gets.chomp
+
+puts "How many items would you like?"
+quantity = gets.chomp.to_i
+
+
 
 
 
