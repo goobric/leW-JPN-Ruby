5572fbc8e222b3466f96d9ce73d9e07e66702a6f
goobric aka Mikal <goobric@gmail.com>
2022-05-20 13:47:19 +0200

    pseudocode the problem

--- 04HmeWrk/store.rb
+++ 04HmeWrk/store.rb
@@ -26,4 +26,13 @@ market_item_prices.each do |item, price|
 end
 # hash is a {placeholder}
 
+### Pseudocode the problem
+##1 Welcome the user to the shop
+##2 Display the items with the prices in the shop
+##3 Ask the user what they want to buy
+##4 Get the user response (item to buy)
+##5 Ask the user for the quantity needed
+##6 Add the items and quantity in the shopping cart
+##7 Display the total bill
+
 
