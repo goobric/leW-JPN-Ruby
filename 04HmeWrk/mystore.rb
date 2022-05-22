14599226b269c0f311118595b6736b16c04a922c
goobric aka Mikal <goobric@gmail.com>
2022-05-22 13:56:14 +0200

    created mystore file

---
+++ 04HmeWrk/mystore.rb
@@ -0,0 +1,8 @@
+### Pseudocode the problem ###
+##1 Welcome the user to the shop
+##2 Display the items with the prices in the shop
+##3 Ask the user what they want to buy
+##4 Get the user response (item to buy)
+##5 Ask the user for the quantity needed
+##6 Add the items and quantity in the shopping cart
+##7 Display the total bill
