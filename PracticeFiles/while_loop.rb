6af131b7011964eb4e795af8c32c5968e05e132f
goobric aka Mikal <goobric@gmail.com>
2022-05-24 09:14:01 +0200

    for loop example

--- PracticeFiles/while_loop.rb
+++ PracticeFiles/while_loop.rb
@@ -92,6 +92,18 @@ end
 
 # In the example above, we wrote the same loop using while and using until.
 
+# Looping with 'For'
+# In case you’re not picking up on the theme of Ruby having a gajillion ways to do any given task: let’s convert our loop yet again.
+
+for k in 1..3
+  print k
+end
+
+# In the above example, we print out 123 by virtue of looping from 1 to 3 inclusive.
+
+for m in 1..50
+    print m
+end
 
 
 
