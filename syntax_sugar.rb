10db97e96860ec188a33898a2f6d8301067adc6d
goobric aka Mikal <goobric@gmail.com>
2022-05-24 13:38:03 +0200

    general if condition

--- syntax_sugar.rb
+++ syntax_sugar.rb
@@ -1,1 +1,15 @@
 ### Syntax Sugar
+# syntactic sugar is syntax within a programming language that is designed to make the code easier to read or express.
+# it makes the code "sweeter" for humans to use
+
+## If Conditions
+
+names = ['doug', 'sasha', 'sylvain', 'aime', 'marlene', 'mika', 'tim', 'raveen', 'nikki', 'olga', 'yukihoshi', 'mai', 'bella', 'tai', 'jane', 'oksana']
+
+if condition
+
+elsif another condition
+
+else
+
+end
