2cc32749ec51b94bd59677b9f537c764a4251fc9
goobric aka Mikal <goobric@gmail.com>
2022-05-24 08:52:42 +0200

    next keyword

--- PracticeFiles/while_loop.rb
+++ PracticeFiles/while_loop.rb
@@ -11,3 +11,15 @@ loop do
   break if i <= 0
 end
 
+# Next!
+
+# The next keyword can be used to skip over certain steps in the loop. For instance, if we don’t want to print out the even numbers, we can write:
+
+for a in 1..5
+  next if a % 2 == 0
+  print a
+end
+
+    # In the above example, we loop through the range of 1 through 5, assigning each number to i in turn.
+    # If the remainder of i / 2 is zero, we go to the next iteration of the loop.
+    # Then we print the value of i. This line only prints out 1, 3, and 5 because of the previous line.
