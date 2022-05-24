55976a5a415518cd5196b5d25bb098224406d914
goobric aka Mikal <goobric@gmail.com>
2022-05-24 09:11:04 +0200

    until loop method

--- PracticeFiles/while_loop.rb
+++ PracticeFiles/while_loop.rb
@@ -76,6 +76,23 @@ while k <51 do
   k += 1
 end
 
+# Looping with 'Until' Good work!
+
+i = 3
+while i > 0 do
+  print i
+  i -= 1
+end
+ 
+j = 3
+until j == 0 do
+  print j
+  j -= 1
+end
+
+# In the example above, we wrote the same loop using while and using until.
+
+
 
 
 
