984fef6bda25bd960a5ee5264fdc2a27ae10ceec
goobric aka Mikal <goobric@gmail.com>
2022-05-24 09:43:45 +0200

    redacted if else example

--- PracticeFiles/daffy_duckie.rb
+++ PracticeFiles/daffy_duckie.rb
@@ -41,3 +41,32 @@ redact = gets.chomp
 
 words = text.split(" ")
 words.each { |word| print word }
+
+# The quick brown fox jumps over the lazy dog
+
+# Control Flow Know-How
+# Good! There were two problems with our output, though: we didn’t have spaces between our words, and our program didn’t actually replace the word we wanted to redact with the word "REDACTED".
+
+if var == 10
+   print "Variable is 10"
+else
+   print "Variable is something else"
+end
+
+# We can fix that with some if/else magic! The above example just reminds you how an if/else block works.
+
+puts "Enter some text: "
+text = gets.chomp
+
+puts "Enter words to redact: "
+redact = gets.chomp
+
+words = text.split(" ")
+words.each { |word| 
+  if word == redact
+    print "REDACTED "
+  else
+ 		print word + " "
+  end }
+
+  
