3981b1bb5c16cf73f2c2faabcd1cda18a069e11a
goobric aka Mikal <goobric@gmail.com>
2022-05-24 15:42:32 +0200

    pseudocode code

---
+++ 05HmeWrk/moonrace.rb
@@ -0,0 +1,11 @@
+### RACE TO THE MOON - a turn based roll-the-dice game
+## Pseudocode
+## Game rules
+# two astronauts are racing to the moon
+# the first to arrive to the moon wins the race
+# 10 steps/spaces are needed to reach the moon
+# the opponent is the computer
+# each player takes a turn to 'roll-the-dice' to step to the moon
+# user starts the race
+
+## PLAN THE GAME
