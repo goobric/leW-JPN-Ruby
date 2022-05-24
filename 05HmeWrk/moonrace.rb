8ae488c06262e151a430723a232e1824e44f0a6a
goobric aka Mikal <goobric@gmail.com>
2022-05-24 15:48:04 +0200

    completed pseudocode

--- 05HmeWrk/moonrace.rb
+++ 05HmeWrk/moonrace.rb
@@ -9,3 +9,13 @@
 # user starts the race
 
 ## PLAN THE GAME
+#1 Greet the user
+#2 ask the user for their name
+#3 store the user name in a variable
+#4 set the user position and computer position to zero
+## user_position = 0
+#5 display the player turn
+#6 get a random number rand(1..6)
+#7 add the random number to the computer's position
+#8 we start the loop (above the dice roll) while/until
+#9 display who won (aka who reached the moon first)
