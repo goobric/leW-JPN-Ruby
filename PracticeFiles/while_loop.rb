deb99afb5eab629afb794c15cd3f9fc832a89335
goobric aka Mikal <goobric@gmail.com>
2022-05-24 09:08:25 +0200

    while do example

--- PracticeFiles/while_loop.rb
+++ PracticeFiles/while_loop.rb
@@ -57,5 +57,26 @@ end
 
 # We don’t know why we would type that, but we could!
 
+# Looping with 'While'
+
+# Okay, training wheels off. Let’s see your stuff!
+
+i = 3
+while i > 0 do
+  print i
+  i -= 1
+end
+
+# In the above example, we create a variable called i and set it to 3.
+# Then, we print out 321 since we execute the loop as long as i is positive.
+
+k = 1
+while k <51 do
+  print k
+  k += 1
+end
+
+
+
 
 
