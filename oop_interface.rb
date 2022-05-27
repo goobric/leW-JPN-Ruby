f08e33eef971a2628601828df30b4eab0ceac34b
goobric aka Mikal <goobric@gmail.com>
2022-05-28 00:04:14 +0200

    role play between warriors

--- oop_interface.rb
+++ oop_interface.rb
@@ -8,10 +8,21 @@ olga = Warrior.new('Olga', Warrior.weapons.keys.sample)
 puts "#{james.name} pulled out a #{james.weapon}!"
 puts "#{olga.name} pulled out a #{jolga.weapon}!"
 
+# create loop to check if a warrior is dead
+until james.dead? || olga.dead?
+    puts "#{james.name} attacks #{olga.name}!"
+    james.attack(olga)
+    puts "#{olga.name} attacks #{james.name}!"
+    olga.attack(james)
+
+    
+    
+end
+
 # p Warrior.weapons
 # p james
 # p olga
 
-olga.attack(james)
-puts james.health
-puts james.dead? # <= 0 is the dead value
+# olga.attack(james)
+# puts james.health
+# puts james.dead? # <= 0 is the dead value
