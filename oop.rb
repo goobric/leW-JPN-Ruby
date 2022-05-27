3e44864a7783c468b6b144ac7e0083820de281bb
goobric aka Mikal <goobric@gmail.com>
2022-05-27 10:02:29 +0200

    what is oop

--- oop.rb
+++ oop.rb
@@ -1,1 +1,8 @@
-#Object Orientating Programming OOP
+###Object Orientating Programming OOP
+
+## Everything in Ruby is an Object
+## Two parts: OOP = Data & Behavior
+
+# Objects are used to store data (state)
+# Methods are used to run behavior
+
