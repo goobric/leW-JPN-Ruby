d11aecf62ea5ac4c932d9d721d1b6f06c75f1e49
goobric aka Mikal <goobric@gmail.com>
2022-05-27 10:05:25 +0200

    method classes

--- oop.rb
+++ oop.rb
@@ -6,3 +6,12 @@
 # Objects are used to store data (state)
 # Methods are used to run behavior
 
+'james' # => is a data String
+
+# What can be done to the data 'james' ?
+'james'.capitalize # => behavior
+'james'.upcase # => behavior
+'james'.downcase # => behavior
+'james'.length # => behavior
+
+# these Methods classes are defined explicitly.
